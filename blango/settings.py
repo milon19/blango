a0a0a55b7cafc6912a64f2878288a0af4d4df8d9
Milon Mahato <milon15-1472@diu.edu.bd>
2022-01-23 10:21:50 +0000

    Finish django registration

--- blango/settings.py
+++ blango/settings.py
@@ -190,3 +190,6 @@ PASSWORD_HASHERS = [
 INTERNAL_IPS = ["192.168.11.179"]
 
 AUTH_USER_MODEL = "blango_auth.User"
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+ACCOUNT_ACTIVATION_DAYS = 7
